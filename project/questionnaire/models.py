74220b1cdfc23380b26d3d7d7fb5676a8af22d16
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-27 16:57:17 +0700

    Added questionnaire_name to model field for more contextual displaying of list of questionnaires.

--- project/questionnaire/models.py
+++ project/questionnaire/models.py
@@ -4,4 +4,5 @@ from django.contrib.postgres.fields import JSONField
 
 class Questionnaire(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
+    questionnaire_name = models.CharField(max_length=255)
     questions = JSONField()
